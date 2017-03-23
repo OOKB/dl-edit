36316b51da837964d238af29d3c049d711aeaa7e
Kristian Bjornard <kb@ookb.co>
2017-03-23 00:08:50 -0400

    okay, playing around with the site nav

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -4,13 +4,13 @@ import css from '../style'
 import LinkEl from './Link'
 
 const styles = {
-  links: css('b1 br1 p1 block fs1 textReset'),
+  links: css('bb block fs1 textReset'),
 }
 
 function NavItem({ isActive, id, ...props }) {
   return (
     <li className={classnames(id, { active: isActive })}>
-      <LinkEl internal {...props} style={styles.links} />
+      <LinkEl className="brown bg-darkgray-hover white-hover" internal {...props} style={styles.links} />
     </li>
   )
 }
