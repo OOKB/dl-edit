01c6f97a70886c3990d90a81599e930199666294
Kai Curry <kai@cape.io>
2017-04-01 15:30:40 -0400

    start Drawer component

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,4 +1,14 @@
-<div>
-  {title && <h2>{title}</h2>}
-  {image && <img src={image} alt={title} />}
-</div>
+import React, { PropType } from 'react'
+
+export default function Drawer({ image, title }) {
+  return (
+    <div>
+      {title && <h2>{title}</h2>}
+      {image && <img src={image} alt={title} />}
+    </div>
+  )
+}
+Drawer.propTypes = {
+  title: PropType.string,
+  image: PropType.string,
+}
